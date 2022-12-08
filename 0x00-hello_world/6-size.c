b8767065a1b0eb341bbb1cc189193ccb18c683aa
salaheddine201 <84785012+salaheddine201@users.noreply.github.com>
2022-12-09 00:13:22 +0100

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ccharacter;
+	int iinteger;
+	long llong;
+	long long llonglong;
+	float ffloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(ccharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(iinteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(llong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(ffloat));
+
+	return (0);
+}
