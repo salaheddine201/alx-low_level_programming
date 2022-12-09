6c92617177548af752163040074acbb3b0519bc1
salaheddine201 <84785012+salaheddine201@users.noreply.github.com>
2022-12-09 23:26:44 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@ include <stdio.h>
 */
 int main(void)
 {
-		int number;
+int number;
 
-		for (number = 0; number <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		}
-		putchar('\n');
+for (number = 0; number <= 9; number++)
+{
+putchar(number + '0');
+if (number < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
